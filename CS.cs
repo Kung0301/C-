d09c994eb45bf931960fc6ec201f4c3fc21dd26d
Kung0301 <kung.positive.mindset@gmail.com>
2024-07-05 21:34:50 +0700

    update

--- CS.cs
+++ CS.cs
@@ -1,16 +1,28 @@
 using System;
 
-namespace ConsoleApp1
+namespace CS
 {
-    class Program
+    /*class ConsoleApp1
     {
-        public void Main(string[] args)
+        static void Main(string[] args)
         {
             string firstName = Console.ReadLine();
             string lastName = Console.ReadLine();
             int age = int.Parse(Console.ReadLine());
             double salary = double.Parse(Console.ReadLine()) + 100;
             Console.WriteLine(firstName + " " + lastName + " " + "Salary :" + " " + salary.ToString() + " " + "THB");
+            Console.ReadLine();
+        }
+    }*/
+    class Condition
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            if(10==10)
+            {
+                Console.WriteLine("True");
+            }
             Console.ReadLine();
         }
     }
